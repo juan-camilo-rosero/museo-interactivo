540170904f49ad6cde98fce4b7e7b9848932e527
juan-camilo-rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2023-08-25 10:35:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ $start_Screen = d.querySelector("#start-screen")
 
 $start_Btn.addEventListener("click", e => {
     $start_Screen.classList.add("hidden")
+    setTimeout(() => {
+        $start_Screen.classList.add("none")
+    }, 500);
 })
